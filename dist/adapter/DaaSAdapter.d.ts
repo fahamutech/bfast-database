cbfc46deb9a00b3ddd2d01627b6510d7e073d6e0
joshua <mama27j@gmail.com>
2020-05-16 20:04:58 +0300

    add storage rest endpoint to support upload file via rest

--- dist/adapter/DaaSAdapter.d.ts
+++
@@ -1,5 +0,0 @@
-import { ConfigAdapter } from "../config";
-export interface DaaSAdapter {
-    start(config: ConfigAdapter): Promise<boolean>;
-    stop(): Promise<boolean>;
-}
--- dist/adapter/RestAdapter.d.ts
+++
@@ -1,8 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-export interface RestAdapter {
-    verifyApplicationId(request: Request, response: Response, next: NextFunction): any;
-    verifyToken(request: Request, response: Response, next: NextFunction): any;
-    verifyMethod(request: Request, response: Response, next: NextFunction): any;
-    verifyBodyData(request: Request, response: Response, next: NextFunction): any;
-    handleRuleBlocks(request: Request, response: Response, next: NextFunction): any;
-}
