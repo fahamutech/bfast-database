e0a59fba5475d2ce4a6ce94a4b4a94c063cf6c53
joshua mshana <mama27j@gmail.com>
2021-01-30 11:50:13 +0300

    update build script

--- build-database-instance.sh
+++ build-database-instance.sh
@@ -1,3 +1,4 @@
 docker build --tag build-db .
 docker run --rm -v "${PWD}":/bfast build-db npm install
-npm publish --registry=https://registry.npmjs.org/:_authToken="${NPM_TOKEN}"
+npm config set //registry.npmjs.org/:_authToken="${NPM_TOKEN}"
+npm publish
